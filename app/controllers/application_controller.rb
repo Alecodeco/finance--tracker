51b484df519dec5788b6d8e07e96bb24f88d4ec9
Alejandro Codeseira <acodeseira@ATM.Local>
2020-03-02 12:57:59 -0300

    User-stock association. *Minor style modifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  helper_method :logged_in?
+
+  def logged_in?
+    true
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :user_stocks
+  
   devise_for :users
   root 'welcome#index'
 
