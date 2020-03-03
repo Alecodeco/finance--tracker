a60b4a91c92782d20724bd1b81abe1a5a2f8bdbd
Alejandro Codeseira <acodeseira@ATM.Local>
2020-03-03 15:17:00 -0300

    Add Names to user, new and edit. * Names on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-
   helper_method :logged_in?
 
-  def logged_in?
-    true
-  end
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
 end
