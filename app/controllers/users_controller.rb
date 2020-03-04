422de43216bd6f97da6fad72cb3096ca2282ee6e
Ale Code <acodeseira@ATM.Local>
2020-03-04 16:22:27 -0300

    User following and search users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
-class UsersController < ApplicationController  
+class UsersController < ApplicationController
 
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friend_list = current_user.friends
+  end
+
+  def search
+    if params[:first_name].present? && params[:last_name].present?
+      @user = User.find_by(first_name: params[:first_name], last_name: params[:last_name])
+      if @user && @user != current_user
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
+      elsif @user && @user == current_user
+        respond_to do |format|
+          @user = nil
+          flash.now[:alert] = "That's yourself !"
+          format.js { render partial: 'users/result'}
+        end
+      elsif !@user
+        respond_to do |format|
+          flash.now[:alert] = "User not found or inexistent"
+          format.js { render partial: 'users/result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter both Name and Last name to search, or search by Email."
+        format.js { render partial: 'users/result'}
+      end
+    end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -22,7 +24,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    return "#{first_name} #{last_name}" if first_name || last_name    
+    return "#{first_name} #{last_name}" if first_name || last_name
     "Anonimous"
   end
 
